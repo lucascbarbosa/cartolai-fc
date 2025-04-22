5bc1ba23960bc64308a178d5cefb04d9d1b8ddfe
lucasbarbosa0899 <lucas.barbosa.0899@poli.ufrj.br>
2025-04-22 10:06:32 -0300

    improved database and score prediction

--- data/fetch_database.py
+++ data/fetch_database.py
@@ -15,128 +15,169 @@ RODADA = args.rodada
 
 #############
 # Functions #
-def _fetch_rodada(rodada_id):
-    """Processa base de rodada."""
+def fetch__atletas__description():
+    """Prepara a base de descrição dos atletas."""
+    mercado_data = requests.get(mercado_url).json()
+    clubes_df = pd.DataFrame(
+        mercado_data['clubes']).T[['id', 'nome_fantasia']].rename(
+            columns={'nome_fantasia': 'clube', 'id': 'clube_id'})
+    posicoes_df = pd.DataFrame(
+        mercado_data['posicoes']).T[['id', 'nome']].rename(
+            columns={'nome': 'posicao', 'id': 'posicao_id'})
+    atletas_df = pd.DataFrame(mercado_data['atletas'])[[
+        'atleta_id', 'clube_id', 'posicao_id', 'apelido'
+    ]]
+    atletas_df['atleta_id'] = atletas_df['atleta_id'].astype(int)
+    atletas_df['clube_id'] = atletas_df['clube_id'].astype(int)
+    atletas_df['posicao_id'] = atletas_df['posicao_id'].astype(int)
+    atletas_df = atletas_df.merge(posicoes_df, on='posicao_id')
+    atletas_df = atletas_df.merge(clubes_df, on='clube_id')
+    atletas_df = atletas_df[[
+        'atleta_id', 'apelido', 'clube_id', 'clube', 'posicao']]
+    return atletas_df
+
+
+def fetch__partidas_clubes__rodada(rodada_id):
+    """Prepara a base de partidas de clubes por rodada."""
+    def _calcular_aproveitamento(resultados):
+        """Calcula aproveitamento entre 0 e 1."""
+        pontos = 0
+        for r in resultados:
+            if r == 'v':
+                pontos += 3
+            elif r == 'e':
+                pontos += 1
+        return pontos / 15
     try:
         partidas_data = requests.get(
             partidas_url.format(rodada=rodada_id)).json()
         partidas_df = pd.DataFrame(
-            partidas_data['partidas'])[['clube_casa_id', 'clube_visitante_id']]
-
-        rodada_df = gatomestre_df.reset_index().rename(
-            columns={"index": "atleta_id"})
-        rodada_df['atleta_id'] = rodada_df['atleta_id'].astype(int)
-        rodada_df = rodada_df.merge(atletas_df, on='atleta_id')
-        rodada_df = rodada_df.merge(
-            status_df, left_on='status_id', right_on='id')
-        rodada_df = rodada_df.merge(
-            posicoes_df, left_on='posicao_id', right_on='id')
-        rodada_df = rodada_df.merge(
-            clubes_df, left_on='clube_id', right_on='id')
-        rodada_df['clube_id'] = rodada_df['clube_id'].astype(int)
-        rodada_df['is_casa'] = rodada_df['clube_id'].isin(
-            partidas_df['clube_casa_id'])
-
-        rodada_df = rodada_df[[
-            'apelido', 'atleta_id', 'entrou_em_campo', 'status', 'posicao',
-            'clube', 'minimos_para_valorizar', 'scout', 'jogos_num', 'is_casa'
-        ]]
-        rodada_df['rodada_id'] = rodada_id
-
-        return rodada_df
+            partidas_data['partidas'])[[
+                'clube_casa_id', 'clube_visitante_id',
+                'aproveitamento_mandante', 'aproveitamento_visitante',
+                'clube_casa_posicao', 'clube_visitante_posicao',
+            ]]
+        casa_df = partidas_df.rename(
+            columns={
+            'clube_casa_id': 'clube_id',
+            'clube_visitante_id': 'clube_adversario_id',
+            'clube_casa_posicao': 'clube_posicao',
+            'clube_visitante_posicao': 'clube_adversario_posicao',
+            'aproveitamento_mandante': 'clube_aproveitamento',
+            'aproveitamento_visitante': 'clube_adversario_aproveitamento'
+            }
+        )
+        casa_df['is_casa'] = True
+        visitante_df = partidas_df.rename(columns={
+            'clube_visitante_id': 'clube_id',
+            'clube_casa_id': 'clube_adversario_id',
+            'clube_visitante_posicao': 'clube_posicao',
+            'clube_casa_posicao': 'clube_adversario_posicao',
+            'aproveitamento_visitante': 'clube_aproveitamento',
+            'aproveitamento_mandante': 'clube_adversario_aproveitamento'
+        })
+        visitante_df['is_casa'] = False
+        partidas_df = pd.concat([casa_df, visitante_df])
+        partidas_df = partidas_df.drop_duplicates(subset='clube_id')
+        partidas_df['rodada_id'] = rodada_id
+        partidas_df['clube_aproveitamento'] = partidas_df[
+            'clube_aproveitamento'].apply(_calcular_aproveitamento)
+        partidas_df[
+            'clube_adversario_aproveitamento'] = partidas_df[
+            'clube_adversario_aproveitamento'].apply(_calcular_aproveitamento)
+        return partidas_df
     except Exception as e:
         print(f"Erro na rodada {rodada_id}: {e}")
         return pd.DataFrame()
 
 
-# Função para buscar pontuação
-def _fetch_pontuacao(atleta_id):
-    """Processa base de pontuações."""
+def fetch__pontuacao_atletas__rodada(atleta_id):
+    """Processa base de pontuacao de atletas por rodada."""
+    response = requests.get(
+        gatomestre_url.format(atleta_id=atleta_id),
+        headers=auth_header)
     try:
-        pontuacao_data = requests.get(
-            pontuacao_url.format(
-                atleta_id=atleta_id), headers=auth_header).json()
-        pontuacao_df = pd.DataFrame(pontuacao_data).dropna()
-        return pontuacao_df[[
-            'atleta_id', 'rodada_id', 'media', 'pontos', 'preco']]
-    except Exception as e:
-        print(f"Erro pontuação atleta {atleta_id}: {e}")
-        return pd.DataFrame()
+        gatomestre_data = response.json()
+        gatomestre_df = pd.DataFrame(
+            gatomestre_data['rodadas']).rename(
+                columns={
+                    'rodada': 'rodada_id',
+                    'status_pre': 'status'
+                }
+            )
+        gatomestre_df['atleta_id'] = atleta_id
+        if 'scouts' in gatomestre_df:
+            scout_df = pd.json_normalize(gatomestre_df['scouts']).fillna(0)
+            scout_df.columns = ['scout_' + c for c in scout_df.columns]
+            gatomestre_df[scout_df.columns] = scout_df
+            gatomestre_df['atleta_id'] = atleta_id
 
+        # Expand preco
+        if 'preco' in gatomestre_df:
+            preco_df = pd.json_normalize(gatomestre_df['preco']).rename(
+                columns={'num': 'preco', 'valorizacao': 'preco_var'}
+            )[['preco', 'preco_var']]
+            gatomestre_df[preco_df.columns] = preco_df
+
+        # Expand pontos
+        if 'pontos' in gatomestre_df:
+            pontos_df = pd.json_normalize(gatomestre_df['pontos']).rename(
+                columns={'num': 'pontos', 'variacao': 'pontos_var'}
+            )[['pontos', 'pontos_var']]
+            gatomestre_df[pontos_df.columns] = pontos_df
+
+        return gatomestre_df[
+            ['atleta_id', 'rodada_id', 'mpv', 'status', 'entrou_em_campo'] +
+            list(gatomestre_df.filter(like='scout_', axis=1).columns) +
+            list(gatomestre_df.filter(like='preco', axis=1).columns) +
+            list(gatomestre_df.filter(like='pontos', axis=1).columns)
+        ]
+
+    except:
+        return pd.DataFrame()
 
 # URLs e headers
 mercado_url = "https://api.cartola.globo.com/atletas/mercado"
-gatomestre_url = "https://api.cartola.globo.com/auth/gatomestre/atletas"
+gatomestre_url = "https://api.gatomestre.globo.com/api/v2/atletas/{atleta_id}"
 partidas_url = "https://api.cartola.globo.com/partidas/{rodada}"
-pontuacao_url = "https://api.cartola.globo.com/auth/mercado/atleta/{atleta_id}/pontuacao"
 auth_header = {
- "Authorization": "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg5NWIwYmIwLTI4ODMtNDE3MC1hMDY2LTZkMDIwZjkzNGRlMyIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiY2FydG9sYUBhcHBzLmdsb2JvaWQiXSwiYXpwIjoiY2FydG9sYUBhcHBzLmdsb2JvaWQiLCJlbWFpbCI6Imx1Y2FzLmJhcmJvc2EuMDg5OUBnbWFpbC5jb20iLCJleHAiOjE3NDUyNDQ2MzYsImZlZGVyYXRlZF9zaWQiOiIxOTM2ZDAwNDA1ZTZkYmIzY2FhZjg0ZTFmZGUzOWUwZjk0YzMwNTI2NzYyNzY0NTUwNDkzNzYzNzg3MTJkNmQ0MTRmNTI2ZjQ4NzQ0OTUzNjMzNzYyNmI2YjZjNGM1NTRlNmQ2ZjQyNGU0Njc1N2E3NzcwMzg3MDM4Nzg1MjY2MmQ1MDQzNTk2MzU4NGM0MjVmNTY0YzczNjQ0NjU0Mzg2ZTM0NDE2MjUzNTI3MzM0NDg3MjYxMzQ2NjUxNjI2ODMyNDU1YTc5Nzg3Mjc3M2QzZDNhMzAzYTZjNzU2MzYxNzMyZTYyNjE3MjJlMzIzMDMxMzQyZTM5IiwiZnNfaWQiOiJMMFJnYnZFUEk3Y3hxLW1BT1JvSHRJU2M3YmtrbExVTm1vQk5GdXp3cDhwOHhSZi1QQ1ljWExCX1ZMc2RGVDhuNEFiU1JzNEhyYTRmUWJoMkVaeXhydz09IiwiZ2xiaWQiOiIxOTM2ZDAwNDA1ZTZkYmIzY2FhZjg0ZTFmZGUzOWUwZjk0YzMwNTI2NzYyNzY0NTUwNDkzNzYzNzg3MTJkNmQ0MTRmNTI2ZjQ4NzQ0OTUzNjMzNzYyNmI2YjZjNGM1NTRlNmQ2ZjQyNGU0Njc1N2E3NzcwMzg3MDM4Nzg1MjY2MmQ1MDQzNTk2MzU4NGM0MjVmNTY0YzczNjQ0NjU0Mzg2ZTM0NDE2MjUzNTI3MzM0NDg3MjYxMzQ2NjUxNjI2ODMyNDU1YTc5Nzg3Mjc3M2QzZDNhMzAzYTZjNzU2MzYxNzMyZTYyNjE3MjJlMzIzMDMxMzQyZTM5IiwiZ2xvYm9faWQiOiIwMjBjZWI5Yy04ZGE5LTQwN2QtOGMzZi05MzFiNDUyYmNmMTMiLCJpYXQiOjE3NDUyNDEwMzUsImlzcyI6Imh0dHBzOi8vZ29pZGMuZ2xvYm8uY29tL2F1dGgvcmVhbG1zL2dsb2JvLmNvbSIsImp0aSI6IjNiZDhkMWYzLTZiZWYtNGI2Yy05YTM1LTg3Y2YxMDgyZDg1YSIsInByZWZlcnJlZF91c2VybmFtZSI6Imx1Y2FzLmJhci4yMDE0LjkiLCJzY3AiOlsib3BlbmlkIiwicHJvZmlsZSJdLCJzZXNzaW9uX3N0YXRlIjoiM2QwYjgxZGYtOTExNC00Zjg1LTg3MGUtNDIwNTA2OGM5MDIzIiwic2lkIjoiM2QwYjgxZGYtOTExNC00Zjg1LTg3MGUtNDIwNTA2OGM5MDIzIiwic3ViIjoiMDIwY2ViOWMtOGRhOS00MDdkLThjM2YtOTMxYjQ1MmJjZjEzIiwidHlwIjoiQmVhcmVyIn0.mSN9_tBL6QiKOBdKhzHZXX_BncCBBIHUmZ8CFCa7rwCflginPdQNqo7URVVj8L80kzw1u725j2P0X_Pfmj_bAgBEIfT9rnc60KhMpkQ_rpITc1coHjtujfHpFovmTVfEtSq1FRncT5zDdnN80qMYyOGsGc2g1MEpZ3oQmdrZJ_JPOI3O5QnckY7i423RSndi8NQsd3CvtMDrdTaSecpioxnkWqmHI-VVQs9z4ntxCUiF69xkYj2dbG9djPhufu4CNqa5El0P7HfmWLdPQLhLGj_XP8u9Pm85fFtDk1tcRP_D_cTC1fqb2ab7JFQ3ZGi_HfVG0juvBZR8sCG8sd4V_g"
+    "Authorization":
+    "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJXLUppTjhfZXdyWE9uVnJFN2lfOGpIY28yU1R4dEtHZF94aW01R2N4WS1ZIn0.eyJleHAiOjE3NDUzMjg3NzMsImlhdCI6MTc0NTMyNTE3MywiYXV0aF90aW1lIjoxNzQ1MzI1MTczLCJqdGkiOiJjOTAxYTdiOC03ZjA3LTRmZDAtOWI5OS04YzE3ZDExMDgwZDEiLCJpc3MiOiJodHRwczovL2lkLmdsb2JvLmNvbS9hdXRoL3JlYWxtcy9nbG9iby5jb20iLCJzdWIiOiJmOjNjZGVhMWZiLTAwMmYtNDg5ZS1iOWMyLWQ1N2FiYTBhZTQ5NDowMjBjZWI5Yy04ZGE5LTQwN2QtOGMzZi05MzFiNDUyYmNmMTMiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJnYXRvbWVzdHJlLXdlYkBhcHBzLmdsb2JvaWQiLCJub25jZSI6IjZmMmJhNDdiLTFmMDItNDVhMS04ODBjLTQzNmVmYWM0OGVjMiIsInNlc3Npb25fc3RhdGUiOiI1MDkyNzE5NS0xMjA0LTRhYWUtOTc4YS00MDVlMTYzOTJiODEiLCJhY3IiOiIxIiwic2NvcGUiOiJvcGVuaWQgZW1haWwgcHJvZmlsZS1taW4gZ2xiaWQgZnMtaWQgZ2xvYm9pZCIsInNpZCI6IjUwOTI3MTk1LTEyMDQtNGFhZS05NzhhLTQwNWUxNjM5MmI4MSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJnbGJpZCI6IjE5MzZkMDA0MDVlNmRiYjNjYWFmODRlMWZkZTM5ZTBmOTRjMzA1MjY3NjI3NjQ1NTA0OTM3NjM3ODcxMmQ2ZDQxNGY1MjZmNDg3NDQ5NTM2MzM3NjI2YjZiNmM0YzU1NGU2ZDZmNDI0ZTQ2NzU3YTc3NzAzODcwMzg3ODUyNjYyZDUwNDM1OTYzNTg0YzQyNWY1NjRjNzM2NDQ2NTQzODZlMzQ0MTYyNTM1MjczMzQ0ODcyNjEzNDY2NTE2MjY4MzI0NTVhNzk3ODcyNzczZDNkM2EzMDNhNmM3NTYzNjE3MzJlNjI2MTcyMmUzMjMwMzEzNDJlMzkiLCJmc19pZCI6IkwwUmdidkVQSTdjeHEtbUFPUm9IdElTYzdia2tsTFVObW9CTkZ1endwOHA4eFJmLVBDWWNYTEJfVkxzZEZUOG40QWJTUnM0SHJhNGZRYmgyRVp5eHJ3PT0iLCJlbWFpbCI6Imx1Y2FzLmJhcmJvc2EuMDg5OUBnbWFpbC5jb20iLCJnbG9ib19pZCI6IjAyMGNlYjljLThkYTktNDA3ZC04YzNmLTkzMWI0NTJiY2YxMyJ9.N_xhXYtGOD7EfnarzVN8SJUDZIGos2Lwx1nTa1ZUAlr8SmS0fJesoYFR4uD58XUe4fwMsYcT4WF08rjs2PG-BcWBXYw2_t1PxUuRGO2XM92KYd3NhcnvCXm3tBn-oRPguQr5ByHTry7opZthdlTiO0YQZrIqqJAvn95vFOv5ODsVZfJyHPMTCiXBwVtT_hatMfuXv_3h1H6tIO1yPhF2e53JfNIXjUuf_DTBL5uTlU5gl_ppOJBOyZQg9jdJ_3Yl15_eEXd36XOLYfro9LlAlVhnymkI1017PjaT1nY_Q3gTXPY0LJtvWM36aBQlTj38Lin0TX9ije_Gxl2GsbkAbQ"
 }
 
+# Atletas description
+atletas_df = fetch__atletas__description()
 
-# Requisições iniciais
-mercado_data = requests.get(mercado_url).json()
-gatomestre_data = requests.get(gatomestre_url, headers=auth_header).json()
-
-# Dataframes fixos
-clubes_df = pd.DataFrame(
-    mercado_data['clubes']).T[['id', 'nome_fantasia']].rename(
-        columns={'nome_fantasia': 'clube'})
-posicoes_df = pd.DataFrame(
-    mercado_data['posicoes']).T[['id', 'nome']].rename(
-        columns={'nome': 'posicao'})
-status_df = pd.DataFrame(
-mercado_data['status']).T[['id', 'nome']].rename(columns={'nome': 'status'})
-atletas_df = pd.DataFrame(mercado_data['atletas'])
-gatomestre_df = pd.DataFrame(gatomestre_data).T
-
+# Partidas rodada
 with ThreadPoolExecutor(max_workers=4) as executor:
-    list__rodada_df = list(
-        tqdm(executor.map(_fetch_rodada, range(1, RODADA + 1)),
+    list__partida_df = list(
+        tqdm(executor.map(fetch__partidas_clubes__rodada, range(1, RODADA + 1)),
         total=RODADA, desc="Rodadas"))
+partidas_df = pd.concat(list__partida_df)
 
-rodadas_df = pd.concat(list__rodada_df)
-
-atletas_ids = rodadas_df['atleta_id'].unique()
-with ThreadPoolExecutor(max_workers=16) as executor:
+# Pontuacoes rodada
+atletas_ids = atletas_df[
+    atletas_df['posicao'] != 'Técnico'
+    ]['atleta_id'].unique()
+with ThreadPoolExecutor(max_workers=8) as executor:
     list__pontuacao_df = list(
-        tqdm(executor.map(_fetch_pontuacao, atletas_ids),
+        tqdm(executor.map(fetch__pontuacao_atletas__rodada, atletas_ids),
         total=len(atletas_ids), desc="Pontuação atletas"))
-
 pontuacoes_df = pd.concat(list__pontuacao_df)
 
+# Fillna
+pontuacoes_df[
+    pontuacoes_df.filter(like='scout_', axis=1).columns
+] = pontuacoes_df[
+    pontuacoes_df.filter(like='scout_', axis=1).columns
+].fillna(0.0)
+
 # Merge final
-database = rodadas_df.merge(
-    pontuacoes_df,
-    on=['atleta_id', 'rodada_id'],
-    how='left')
-
-# Expand scout
-scout_df = pd.json_normalize(database['scout']).fillna(0)
-scout_df.columns = ['scout_' + c for c in scout_df.columns]
-database[scout_df.columns] = scout_df
-
-# Expand minimos
-minimos_df = pd.json_normalize(database['minimos_para_valorizar'])
-minimos_df.columns = ['minval_' + c for c in minimos_df.columns]
-database[minimos_df.columns] = minimos_df
-
-# Crie score unico
-database['score'] = (
-    2 * database['media'] -
-    (
-        (
-            database['minval_1'] / 3 +
-            database['minval_2'] / 6 +
-            database['minval_3'] / 9)
+database = pontuacoes_df.merge(
+    atletas_df, on='atleta_id', how='left').merge(
+        partidas_df, on=['rodada_id', 'clube_id'],
+        how='outer'
     )
-) / (0.5 * database['preco'])
-
-# Drop columns
-database = database.drop(
-    ['minimos_para_valorizar', 'scout'],
-    axis=1
-)
 
 # Export
 database.to_excel(f"dados__rodada_{RODADA}.xlsx", index=False)
--- models/predict_score.py
+++ models/predict_score.py
@@ -1,6 +1,7 @@
 """Naive lineup based on score."""
 import argparse
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
@@ -24,16 +25,22 @@ ESQUEMA = args.esquema
 database = pd.read_excel(f"../data/dados__rodada_{RODADA}.xlsx")
 
 # Filter rows
-database = database[
-    (database['entrou_em_campo']) & (database['status'] == 'Provável')]
+database = database[database['entrou_em_campo'] == 1.0].dropna()
 
 # Filter columns
 database = database.drop([
     'apelido', 'atleta_id', 'entrou_em_campo',
-    'status', 'clube', 'rodada_id'], axis=1)
+    'status', 'clube', 'rodada_id', 'clube_id',
+    'clube_adversario_id'], axis=1)
 
 # Encode posicao
 database = pd.get_dummies(database, columns=['posicao'])
+
+# Convert is_casa to int
+database['is_casa'] = database['is_casa'].astype(int)
+
+# Replace '-' in mpv
+database['mpv'] = database['mpv'].replace('-', 0.0).astype(float)
 
 # Normalize
 scaler = MinMaxScaler()
@@ -44,8 +51,8 @@ scaled_database = pd.DataFrame(
 )
 
 # Split X and y
-X = scaled_database.drop(['media'], axis=1)
-y = scaled_database['media']
+X = scaled_database.drop(['pontos'], axis=1)
+y = scaled_database['pontos']
 
 # Split train and test
 TEST_SPLIT = 0.2
@@ -60,4 +67,15 @@ mse = mean_squared_error(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
-print(f"Resultados:\n-MSE: {mse}\n-MAE: {mae}\n-R^2: {r2}")
+print(f"Resultados:\n  MSE: {mse}\n  MAE: {mae}\n  R^2: {r2}")
+plt.figure(figsize=(6, 6))
+plt.scatter(y_test, y_pred, alpha=0.5)
+plt.plot(
+    [min(y_test), max(y_test)], [min(y_test), max(y_test)],
+    color='red',
+    linestyle='--')
+plt.xlabel('Pontuação Real')
+plt.ylabel('Pontuação predita')
+plt.title('Predição de pontuação')
+plt.grid(True)
+plt.show()
